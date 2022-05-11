7451983cf1e5053091a6c954fbd7e763bdb77587
ggzzggzz <ggzz553@gmail.com>
2022-05-11 16:49:30 +0900

    0511

---
+++ src/main/java/org/zerock/domain/ex01/EmployeeDto.java
@@ -0,0 +1,18 @@
+package org.zerock.domain.ex01;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+	private String lastName;
+	private String firstName;
+//	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate birthDate;
+	private String photo;
+	private String notes;
+}
